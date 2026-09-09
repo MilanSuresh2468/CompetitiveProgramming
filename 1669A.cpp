244532483387ad226ca93d636e1c02814238a714
MilanSuresh2468 <73302110+MilanSuresh2468@users.noreply.github.com>
2026-09-08 19:50:46 -0700

    Create 1669A.cpp

---
+++ 1669A.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t, rating;
+    
+    cin >> t;
+    
+    while (t--) {
+        cin >> rating;
+        if (rating >= 1900) {
+            cout << "Division 1\n";
+        } else if (rating >= 1600 && rating <= 1899) {
+            cout << "Division 2\n";
+        } else if (rating >= 1400 && rating <= 1599) {
+            cout << "Division 3\n";
+        } else {
+            cout << "Division 4\n";
+        }
+    }
+    
+    return 0;
+}
