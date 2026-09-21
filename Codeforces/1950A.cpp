484b743c3d261d399b086c87dac5745f2f4e93d0
MilanSuresh2468 <73302110+MilanSuresh2468@users.noreply.github.com>
2026-09-21 16:18:27 -0700

    Create 1950A.cpp

---
+++ Codeforces/1950A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t, a, b, c;
+    
+    cin >> t;
+    while (t--) {
+        cin >> a >> b >> c;
+        if (a < b && b < c) {
+            cout << "STAIR\n";
+        } else if (a < b && b > c) {
+            cout << "PEAK\n";
+        } else {
+            cout << "NONE\n";
+        }
+    }
+
+    return 0;
+}
