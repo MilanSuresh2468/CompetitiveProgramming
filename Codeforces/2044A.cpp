69f8eae421b765ce948a94e9a39b111867ccf061
MilanSuresh2468 <73302110+MilanSuresh2468@users.noreply.github.com>
2026-09-25 10:24:39 -0700

    Create 2044A.cpp

---
+++ Codeforces/2044A.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Type Aliases 
+using ll = long long;
+using db = long double;
+using str = string;
+
+// Vectors
+#define sort(x) sort(begin(x), end(x))
+
+// Algorithms
+bool isPrime(int n) {
+    for (int i = 2; i < sqrt(n); i++) {
+        if (n%i == 0) return false;
+    }
+    return true;
+}
+
+
+int main() {
+    int t, n;
+    cin >> t;
+    
+    while (t--) {
+        cin >> n;
+        cout << n - 1 << endl;
+    }
+    return 0;
+}
